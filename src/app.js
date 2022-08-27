87b6cbf7ff31e64b10377e4e3f3a5e71ca1fc4c9
ladymad <ladymad31116@gmail.com>
2022-08-27 19:10:10 -0400

    link API to temp display

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
+function displayTemp(response) {
+  let temperatureMain = document.querySelector(".temperature");
+  temperatureMain.innerHTML = Math.round(response.data.main.temp);
+}
+let cityName = "charlotte";
 let apiKey = "1eefbb06c86a0d46a195a5f586c20304";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${apiKey}&units=imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemp);
