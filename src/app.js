ace9eed66172e3ae703804102e03eb6a9aa625d1
ladymad <ladymad31116@gmail.com>
2022-08-28 15:53:38 -0400

    update time/date functions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
+function formatDate() {
+  let date = new Date();
+
   let days = [
     "Sunday",
     "Monday",
@@ -11,8 +10,16 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let day = date.getDay();
-  return `${day} ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+  return `${day}`;
+}
+
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  return `${hours}:${minutes}`;
 }
 
 function displayWeather(response) {
@@ -24,6 +31,7 @@ function displayWeather(response) {
   let windElement = document.querySelector(".wind");
   let countryName = document.querySelector("#country-name");
   let dateElement = document.querySelector(".date");
+  let timeElement = document.querySelector(".time");
   temperatureMain.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].main;
@@ -31,6 +39,7 @@ function displayWeather(response) {
   windElement.innerHTML = `${Math.round(response.data.wind.speed)} k/hr`;
   countryName.innerHTML = response.data.sys.country;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  timeElement.innerHTML = formatTime(response.data.dt * 1000);
 }
 let cityName = "Paris";
 let apiKey = "1eefbb06c86a0d46a195a5f586c20304";
