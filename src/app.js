d7b179b1573ad19409a8eadb2a800290050e8436
ladymad <ladymad31116@gmail.com>
2022-08-27 20:08:26 -0400

    condition added to function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 function displayTemp(response) {
+  console.log(response.data);
   let temperatureMain = document.querySelector(".temperature");
+  let cityElement = document.querySelector("#city-name");
+  let conditionElement = document.querySelector(".condition");
   temperatureMain.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  conditionElement.innerHTML = response.data.weather[0].main;
 }
 let cityName = "charlotte";
 let apiKey = "1eefbb06c86a0d46a195a5f586c20304";
