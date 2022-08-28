e13a3cbd411f6fb00d5121954061b73ea5283cfb
ladymad <ladymad31116@gmail.com>
2022-08-27 20:24:18 -0400

    wind/humidity linked to api in html

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ function displayTemp(response) {
   let temperatureMain = document.querySelector(".temperature");
   let cityElement = document.querySelector("#city-name");
   let conditionElement = document.querySelector(".condition");
+  let humidityElement = document.querySelector(".humidity");
+  let windElement = document.querySelector(".wind");
   temperatureMain.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} k/hr`;
 }
 let cityName = "charlotte";
 let apiKey = "1eefbb06c86a0d46a195a5f586c20304";
