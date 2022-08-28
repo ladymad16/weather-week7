26d99f36de7fc6c3dd467b7023970c2732917759
ladymad <ladymad31116@gmail.com>
2022-08-28 15:45:02 -0400

    add time/date function

--- src/app.js
+++ src/app.js
@@ -1,18 +1,39 @@
-function displayTemp(response) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDay();
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeather(response) {
   console.log(response.data);
   let temperatureMain = document.querySelector(".temperature");
   let cityElement = document.querySelector("#city-name");
   let conditionElement = document.querySelector(".condition");
   let humidityElement = document.querySelector(".humidity");
   let windElement = document.querySelector(".wind");
+  let countryName = document.querySelector("#country-name");
+  let dateElement = document.querySelector(".date");
   temperatureMain.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
-  windElement.innerHTML = `${response.data.wind.speed} k/hr`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} k/hr`;
+  countryName.innerHTML = response.data.sys.country;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
-let cityName = "charlotte";
+let cityName = "Paris";
 let apiKey = "1eefbb06c86a0d46a195a5f586c20304";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 
-axios.get(apiUrl).then(displayTemp);
+axios.get(apiUrl).then(displayWeather);
